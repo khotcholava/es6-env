40ce505145dc291b5cae36b289f1b1478db93cfe
khocholava <akokhocholava@gmail.com>
2020-04-26 04:23:57 +0400

    Implemented random color generator and other stuff

--- app/app.js
+++ app/app.js
@@ -1,3 +1,37 @@
-const App = () => `Hello World`;
+import {getRandomColor} from './components/utils';
 
-console.log(App());
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+console.log(innerHeight);
+console.log(innerWidth);
+let x = Math.random() * canvas.width;
+let y = Math.random() * canvas.height;
+let dx = 2;
+let dy = 5;
+let radius = 30;
+let strokeStyle = getRandomColor();
+const animate = () => {
+
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+    ctx.strokeStyle = strokeStyle;
+    ctx.stroke();
+
+    if (x + radius > canvas.width || x - radius < 0) {
+        dx = -dx;
+        strokeStyle = getRandomColor();
+    }
+    if (y + radius > canvas.height || y - radius < 0) {
+        dy = -dy;
+        strokeStyle = getRandomColor();
+
+    }
+
+    x += dx;
+    y += dy;
+};
+
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/,
+            options: {
+                'plugins': ['@babel/plugin-proposal-class-properties']
+            },
         }],
     },
     devServer: {
